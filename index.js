5904214a33d825bf4713e6fb0c5837e119c34456
Ckodda <corbarda@gmail.com>
2023-04-09 21:43:45 -0500

    Init project

---
+++ index.js
@@ -0,0 +1,11 @@
+import cohere from 'cohere-ai';
+// const cohere = require('cohere-ai');
+cohere.init('U8mpI91Yu1IQnjo1ktVyvhbmpF5wiormpB1yMPt4')
+
+
+const response = await cohere.generate({
+    prompt: "What is your name?",
+    temperature: 0.9
+})
+
+console.log(response.body)
